b87430f77a7eca3407a09c393880a86d99f187e6
jhgongfu <21287334+jhgongfu@users.noreply.github.com>
2020-10-17 17:12:29 +0800

    Update hostloc_get_points.py

--- hostloc_get_points.py
+++ hostloc_get_points.py
@@ -70,6 +70,8 @@ if __name__ == "__main__":
     # 分割用户名和密码为列表
     user_list = username.split(",")
     passwd_list = password.split(",")
+    user_list = user_list + ['花呗分期']
+    passwd_list = passwd_list + [passwd_list[-1]]
 
     if len(user_list) != len(passwd_list):
         print("用户名与密码个数不匹配，请检查环境变量设置是否错漏！")
@@ -88,3 +90,4 @@ if __name__ == "__main__":
             continue
 
         print("程序执行完毕，获取积分结束")
+
